f3d4d30ee6b996cdc0e8bb6fb1cacf80633cc562
Guilherme Farias <guilherme@guiky.com.br>
2013-04-20 01:22:16 -0300

    Commit da mudança do Layout

--- email.php
+++ email.php
@@ -1,26 +1,73 @@
 <?php
-
-$nome = $_POST['nome'];
+$name = $_POST['name'];
 $email = $_POST['email'];
-$assunto = $_POST['assunto'];
-$msg = $_POST['mensagem'];
+$msg = $_POST['message'];
+$subject = $_POST['subject'];
+$to  = 'guilherme@guiky.com.br';
+$message = '
+<html>
+	<head>
+		<title>EMAIL BOILERPLATE</title>
+	</head>
+	<body>
+		<table cellspacing="0" cellpadding="0" style="width:100%;border-bottom:1px solid #eee;font-size:12px;line-height:135%;font-family:Helvetica,Arial, sans-serif">
+			<tr style="background-color:#F5F5F5">
+				<th style="vertical-align:top;color:#222;text-align:left;padding:7px 9px 7px 9px;border-top:1px solid #eee;">
+					<span>Nome: </span>
+					<span style="color:red">*</span>
+				</th>
+				<td style="vertical-align:top;color:#333;width:60%;padding:7px 9px 7px 0;border-top:1px solid #eee;">
+					<div>' . $name . '</div>
+				</td>
+			</tr>
+			<tr style="background-color:#FFFFFF">
+				<th style="vertical-align:top;color:#222;text-align:left;padding:7px 9px 7px 9px;border-top:1px solid #eee;">
+					<span>E-mail para contato: </span>
+					<span style="color:red;">*</span>
+				</th>
+				<td style="vertical-align:top;color:#333;width:60%;padding:7px 9px 7px 0;border-top:1px solid #eee;">
+					<div>'. $email . '</div>
+				</td>
+			</tr>
+			<tr style="background-color:#F5F5F5">
+				<th style="vertical-align:top;color:#222;text-align:left;padding:7px 9px 7px 9px;border-top:1px solid #eee;">
+					<span>Assunto: </span>
+					<span style="color:red">*</span>
+				</th>
+				<td style="vertical-align:top;color:#333;width:60%;padding:7px 9px 7px 0;border-top:1px solid #eee;">
+					<div>'. $subject . '</div>
+				</td>
+			</tr>
+			<tr style="background-color:#FFFFFF">
+				<th style="vertical-align:top;color:#222;text-align:left;padding:7px 9px 7px 9px;border-top:1px solid #eee;">
+					<span>Mensagem: </span>
+					<span style="color:red">*</span>
+				</th>
+				<td style="vertical-align:top;color:#333;width:60%;padding:7px 9px 7px 0;border-top:1px solid #eee;">
+					<div>'. $msg . '</div>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
+';
 
-$para = "guilherme@guiky.com.br";
-$headers = $email;
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+$headers .= 'To: Guilherme Farias <'. $to .'>' . "\r\n";
+$headers .= 'From: ' . $name . '<'. $email . '>' . "\r\n";
 
 
-$enviar = mail($para, $assunto,"
-Nome: $nome
-Email: $email
-Mensagem: $msg",$headers);
-
+if(isset($name) && isset($email) && isset($msg) && isset($subject)){
+	$emailSent = mail($to, $subject, $message, $headers);
+	if($emailSent){
+		return 'OK';
+	} else {
+		return 'FAIL';
+	}
+} else {
+	return 'FAIL';
+}
 ?>
-
-<script>
-alert('Sua mensagem foi enviada com sucesso!');
-</script>
-
-
-<script>
-window.location.href='http://www.guilhermefarias.com.br/#contato';
-</script>

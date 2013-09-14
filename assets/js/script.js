73ca020cfe8ae811f087147d8672b48b649bf6a3
Guilherme Farias <guilherme@guiky.com.br>
2013-09-14 14:44:43 -0300

    Organizing files directories

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ var Guilherme = {
 				'subject=' + encodeURIComponent(formElement.subject.value)+'&'+
 				'message=' + encodeURIComponent(formElement.message.value);
 
-		submitRequest.open("POST","email.php",true);
+		submitRequest.open("POST","assets/php/email.php",true);
 		submitRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		submitRequest.onreadystatechange = function(){
 			if (submitRequest.readyState == 4 && submitRequest.status == 200 && submitRequest.responseText == 'OK'){
@@ -91,7 +91,7 @@ var Guilherme = {
 			'<h2>Palestras</h2>'+
 
 			'<div class="talks">'+
-				'<div class="img"><img src="img/talks/03.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/talks/03.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>2° Open Meeting - PernambucoJS</h3>'+
 					'<p><b>Palestra:</b> Getting Started with FirefoxOS</p>'+
@@ -102,7 +102,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="talks">'+
-				'<div class="img"><img src="img/talks/02.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/talks/02.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>MobDev Alagoas</h3>'+
 					'<p><b>Palestra:</b> FirefoxOS - A web como plataforma, usando HTML5 e Web Standards</p>'+
@@ -113,7 +113,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="talks">'+
-				'<div class="img"><img src="img/talks/01.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/talks/01.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>FEEC Brazil - Recife</h3>'+
 					'<p><b>Palestra:</b> Aplicações hibridas usando Phonegap</p>'+
@@ -127,7 +127,7 @@ var Guilherme = {
 			'<h2>Projetos</h2>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/07.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/07.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Dado em CSS</h3>'+
 					'<p>Dado feito apenas com CSS3.<br/> Veja como é possivel fazer um dado 3D e com animações usando apenas CSS e um pouco de HTML</p><br>'+
@@ -136,7 +136,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/05.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/05.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Zoologico do Tico</h3>'+
 					'<p>É um jogo educativo onde a criança deve reconher os animais exibidos na tela, o jogo foi totalmente desenvolvido com os novos recursos do HTML5. Devido ao uso da API de reconhecimento de voz do Google, o jogo só está disponivel para o Google Chrome</p><br>'+
@@ -145,7 +145,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/09.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/09.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>CSSReload.JS</h3>'+
 					'<p>Script que recarrega todos os arquivos CSS da página em um determinado intervalo de tempo. Feito apenas com JavaScript (no jQuery dependency)</p><br>'+
@@ -154,7 +154,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/08.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/08.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>ZeptoJS BR</h3>'+
 					'<p>Tradução da documentação do ZeptoJS para o português. Zepto é uma biblioteca JavaScript minimalista para navegadores modernos com uma API em grande parte compatível com jQuery.</p><br>'+
@@ -163,7 +163,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/10.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/10.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Fear Ant</h3>'+
 					'<p>Exemplo de uso dos Ambient Light Events, onde as formigas (que foram feitas apenas com CSS3) se movem ao estarem em um ambiente com pouca luz (é recomendado que o dispositivo tenha sensor de luminosidade para melhor experiência)</p><br>'+
@@ -172,7 +172,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/06.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/06.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Educca</h3>'+
 					'<p>É um sistema de gestão educacional com o objetivo modernizar a maneira como os alunos se relacionam com as instituições de ensinos. Facilitar e diminuir custos com comunicação, melhorar a gestão das escolas ou faculdades são alguns dos objetivos do sistema.</p><br>'+
@@ -181,7 +181,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/04.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/04.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Meus Gastos</h3>'+
 					'<p>Meus Gastos é um gerenciador financeiro online, onde você pode registrar seus ganhos e seus gastos, tendo um maior controle do seu dinheiro.</p><br>'+
@@ -190,7 +190,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/03.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/03.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Gnoteboard</h3>'+
 					'<p>Aplicativo para organizar anotações pessoais, você pode organizar suas notas como um mural de post-it.</p><br>'+
@@ -199,7 +199,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/02.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/02.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>NodeJS Chat</h3>'+
 					'<p>Chat feito em NodeJS onde é possivel criar salas de bate-papo com vários usuários e trocar mensagem em tempo real utilizando Web Sockets.</p><br>'+
@@ -208,7 +208,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/projetos/01.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/projetos/01.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Jogo da Velha</h3>'+
 					'<p> Esta é a versão inicial do jogo da velha, por enquanto os 2 jogadores só podem jogar na mesma tela, em breve será possivel jogar sozinho e até mesmo jogar com outra pessoa que está em outro computador, o jogo será em tempo real, utilizando WebSockets e NodeJS.</p><br>'+
@@ -220,7 +220,7 @@ var Guilherme = {
 			'<h2>Sites</h2>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/15.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/15.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>LCM Comunicação</h3>'+
 					'<p>Site da empresa LCM Comunicação, desenvolvido em WordPress, por mim no Estudio Zenit.</p><br>'+
@@ -229,7 +229,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/14.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/14.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Proservil</h3>'+
 					'<p>Site da empresa Proservil, desenvolvido em WordPress, por mim na Chama Comunicação.</p><br>'+
@@ -238,7 +238,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/17.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/17.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Revista da Cidade</h3>'+
 					'<p>Portal da Revista da Cidade, o guia comercial da cidade de São Lourenço da Mata e região, desenvolvido em WordPress, por mim na Chama Comunicação.</p><br>'+
@@ -247,7 +247,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/13.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/13.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Boneca de Platina</h3>'+
 					'<p>Blog Boneca de Platina, desenvolvido por mim e Dash e Dobbin no Estudio Zenit.</p><br>'+
@@ -256,7 +256,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/18.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/18.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Cimentazzi</h3>'+
 					'<p>Site da empresa Cimentazzi, desenvolvido em WordPress, por mim na Chama Comunicação.</p><br>'+
@@ -265,7 +265,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/16.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/16.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Bolo Master</h3>'+
 					'<p>Site da empresa de bolos Bolo Master, desenvolvido em WordPress, por mim na Chama Comunicação.</p><br>'+
@@ -274,7 +274,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/12.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/12.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Inforline Cursos</h3>'+
 					'<p>Site da Inforline Cursos, desenvolvido por mim na Chama Comunicação utilizando o CMS WordPress.</p><br>'+
@@ -283,7 +283,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/11.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/11.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Pousada Olho DAgua</h3>'+
 					'<p>Site da pousada Olho DAgua, Maragogi - AL. Desenvolvido em conjunto com Lucas Cavalcanti, utilizando o CMS WordPress.</p><br>'+
@@ -292,7 +292,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/10.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/10.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Vidda</h3>'+
 					'<p>Site da marca de cosmeticos Vidda, o layout foi feito pela agência COL e enviado em .PSD, o restante foi desenvolvido popor mim, utilizando o CMS WordPress.</p><br>'+
@@ -301,7 +301,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/09.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/09.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Farmaervas</h3>'+
 					'<p>Site instituicional da empresa de cosmeticos Farmaervas, o site inteiro foi feito em WordPress, a programação feita por mim e o design do site foi desenvolvido pela agência COL.</p><br>'+
@@ -310,7 +310,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/08.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/08.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Guiky</h3>'+
 					'<p>Nova versão do meu blog de Tecnologia Guiky, totalmente desenvolvido por mim, utilizando o CMS WordPress, HTML5, CSS3, Javascrtip/jQuery e etc.</p><br>'+
@@ -319,7 +319,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/19.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/19.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Educca</h3>'+
 					'<p>Site instituicional da startup Educca, responsável por criar um sistema de gestão educacional online voltado para escolas, faculdades e cursos técnicos.</p><br>'+
@@ -328,7 +328,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/05.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/05.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Fashion Fun</h3>'+
 					'<p>Layout foi enviado em .PSD, feito pela agência COL, o site foi desenvolvido por mim, utilizando o CMS WordPress.</p><br>'+
@@ -337,7 +337,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/07.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/07.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>COL - Consultoria Online</h3>'+
 					'<p>Site da COL, empresa de consultoria de comunicação e imagem especializada em ambiente online.</p><br>'+
@@ -346,7 +346,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/20.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/20.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Guiky Labs Host</h3>'+
 					'<p>Site instituicional da minha empresa de hospedagem.</p><br>'+
@@ -355,7 +355,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/06.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/06.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Multitask Girl</h3>'+
 					'<p>Blog Multitask Girl desenvolvido em WordPress.</p><br>'+
@@ -366,7 +366,7 @@ var Guilherme = {
 
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/04.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/04.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>RBMS Representações</h3>'+
 					'<p>Site da empresa RBMS Representações, desenvolvido em WordPress.</p><br>'+
@@ -375,7 +375,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/03.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/03.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Powerkiteboat</h3>'+
 					'<p>Projeto do barco Tucamarino, site desenvolvido em WordPress por mim e Yeltsin Lima</p><br>'+
@@ -384,7 +384,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/02.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/02.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Projeto Ecocidades</h3>'+
 					'<p>Site desenvolvido para conclusão do curso de webdesign no programa da Microsoft, Students to Business.</p><br>'+
@@ -392,7 +392,7 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="app">'+
-				'<div class="img"><img src="img/sites/01.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/sites/01.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Portfolio de Anderson Santos</h3>'+
 					'<p>Porfolio de Anderson Santos, estudante de Jogos Digitais</p><br>'+
@@ -403,8 +403,8 @@ var Guilherme = {
 			'<h2>Mobile</h2>'+
 
 			'<div class="mobile">'+
-				'<div class="img"><img src="img/mobile/03-a.jpg" /></div>'+
-				'<div class="img"><img src="img/mobile/03-b.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/mobile/03-a.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/mobile/03-b.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Cahier</h3>'+
 					'<p>Aplicativo da rede social corporativa Cahier, feito para iPhone e iPad utilizando o framework Phonegap, projeto desenvolvido por mim e <a href="http://luiztiago.com" target="_blank">Luiz Tiago</a></p><br>'+
@@ -413,8 +413,8 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="mobile">'+
-				'<div class="img"><img src="img/mobile/02-a.jpg" /></div>'+
-				'<div class="img"><img src="img/mobile/02-b.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/mobile/02-a.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/mobile/02-b.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>LCM Comunicação</h3>'+
 					'<p>WebApp da LCM Comunicação, onde o usuário pode conhecer a empresa, além de poder ver fotos, portfolio e ainda entrar em contato com a equipe!</p><br>'+
@@ -423,8 +423,8 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="mobile">'+
-				'<div class="img"><img src="img/mobile/01-a.jpg" /></div>'+
-				'<div class="img"><img src="img/mobile/01-b.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/mobile/01-a.jpg" /></div>'+
+				'<div class="img"><img src="assets/img/mobile/01-b.jpg" /></div>'+
 				'<div class="description">'+
 					'<h3>Gnoteboard</h3>'+
 					'<p>Aplicativo para organizar anotações pessoais para Android, você pode organizar suas notas como um mural de post-it.</p><br>'+

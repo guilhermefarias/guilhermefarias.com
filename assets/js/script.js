3933a4cd9318a7bdf2b60ea439acf0d704aa296c
Guilherme Farias <guilherme@guiky.com.br>
2014-03-20 15:00:32 -0300

    Adding Embalar to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -465,6 +465,11 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="site item">'+
+				'<div class="item-img"><img src="assets/img/sites/33.jpg" /></div>'+
+				'<a class="item-title" href="#">Embalar</a>'+
+			'</div>'+
+
+			'<div class="site item">'+
 				'<div class="item-img"><img src="assets/img/sites/26.jpg" /></div>'+
 				'<a class="item-title" href="http://somdorio.com.br/" target="_blank">Festival Som do Rio</a>'+
 			'</div>' +

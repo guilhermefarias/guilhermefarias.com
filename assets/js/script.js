f1204958084402f8eeadb590e0d59c9d0a47e48e
Guilherme Farias <guilherme@guiky.com.br>
2014-03-25 10:01:36 -0300

    Adding JKL Stands to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -490,6 +490,11 @@ var Guilherme = {
 			'</div>' +
 
 			'<div class="site item">'+
+				'<div class="item-img"><img src="assets/img/sites/34.jpg" /></div>'+
+				'<a class="item-title" href="http://jklstands.com.br" target="_blank">JKL Stands</a>'+
+			'</div>' +
+
+			'<div class="site item">'+
 				'<div class="item-img"><img src="assets/img/sites/30.jpg" /></div>'+
 				'<a class="item-title" href="http://mpn.adv.br" target="_blank">MPN Advogados</a>'+
 			'</div>' +

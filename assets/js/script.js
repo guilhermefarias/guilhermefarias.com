fdfccfabb769668ae7dd90b76402419e738b3d39
Guilherme Farias <guilherme@guiky.com.br>
2014-06-25 03:12:15 -0300

    Adding SNNE to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -581,6 +581,11 @@ var Guilherme = {
 			'</div>' +
 
 			'<div class="site item">'+
+				'<div class="item-img"><img src="assets/img/sites/41.jpg" /></div>'+
+				'<a class="item-title" href="http://snne.com.br/" target="_blank">SNNE</a>'+
+			'</div>' +
+
+			'<div class="site item">'+
 				'<div class="item-img"><img src="assets/img/sites/36.jpg" /></div>'+
 				'<a class="item-title" href="http://jetservicecomercial.com" target="_blank">Jet Service</a>'+
 			'</div>' +

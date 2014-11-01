b11f4687c127d867d6e01460030cb51b2e45c7ad
Guilherme Farias <guilherme@guiky.com.br>
2014-11-01 00:55:49 -0300

    Adding SoproVerde to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -611,6 +611,11 @@ var Guilherme = {
 			'</div>' +
 
 			'<div class="site item">'+
+				'<div class="item-img"><img src="assets/img/sites/46.jpg" /></div>'+
+				'<a class="item-title" href="http://soproverde.com.br/" target="_blank">Sopro Verde</a>'+
+			'</div>' +
+
+			'<div class="site item">'+
 				'<div class="item-img"><img src="assets/img/sites/42.jpg" /></div>'+
 				'<a class="item-title" href="http://chamacomunicacao.com.br/" target="_blank">Chama Comunicação</a>'+
 			'</div>' +

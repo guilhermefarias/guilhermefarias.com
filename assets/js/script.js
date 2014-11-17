673b57f9d04072c791be801396b12f1635a52e60
Guilherme Farias <guilherme@guiky.com.br>
2014-11-17 01:24:30 -0300

    Adding Rede RenoveJa to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -631,6 +631,11 @@ var Guilherme = {
 			'</div>' +
 
 			'<div class="site item">'+
+				'<div class="item-img"><img src="assets/img/sites/50.jpg" /></div>'+
+				'<a class="item-title" href="http://rederenoveja.com.br/" target="_blank">Rede RenoveJá</a>'+
+			'</div>' +
+
+			'<div class="site item">'+
 				'<div class="item-img"><img src="assets/img/sites/42.jpg" /></div>'+
 				'<a class="item-title" href="http://chamacomunicacao.com.br/" target="_blank">Chama Comunicação</a>'+
 			'</div>' +

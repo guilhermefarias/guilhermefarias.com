aaf69d31fbe258996f78d2929ad9f29076520899
Guilherme Farias <guilherme@guiky.com.br>
2015-03-19 03:06:44 -0300

    Adding VIP Pilates to portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -565,6 +565,11 @@ var Guilherme = {
 			'</div>'+
 
 			'<div class="site item">'+
+				'<div class="item-img"><img src="assets/img/sites/53.jpg" /></div>'+
+				'<a class="item-title" target="_blank">Vip Pilates</a>'+
+			'</div>'+
+
+			'<div class="site item">'+
 				'<div class="item-img"><img src="assets/img/sites/26.jpg" /></div>'+
 				'<a class="item-title" href="http://somdorio.com.br/" target="_blank">Festival Som do Rio</a>'+
 			'</div>' +
